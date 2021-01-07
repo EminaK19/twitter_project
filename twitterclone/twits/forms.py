1948f48ca92752616fa6e0a82f925c99a696b78c
EminaK19 <kuramaeva.emina@mail.ru>
2021-01-07 19:10:15 +0600

    added twit app

---
+++ twitterclone/twits/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+from .models import Tweet
+
+
+class AddPostForm(forms.ModelForm):
+    class Meta:
+        model = Tweet
+        fields = ('tweet_text', 'tweet_image')
+        exclude = ('created_at', 'updated_at')
+
+
+class UpdatePostForm(forms.ModelForm):
+    class Meta:
+        model = Tweet
+        fields = ('tweet_text', 'tweet_image')
--- twitterclone/twits/models.py
+++ twitterclone/twits/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# Create your models here.
+
+
+class Tweet(models.Model):
+    tweet_text = models.TextField(blank=True, null=True)
+    tweet_image = models.ImageField(upload_to='tweets', null=True, blank=True)
+    author = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, related_name='tweets'
+        )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
