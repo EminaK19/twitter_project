567385975379cc52658ea30d5e990a64a3f0abba
Beki <akmatovabeka21@gmail.com>
2021-01-09 18:07:16 +0600

    auth

--- twitterclone/twitterclone/settings.py
+++ twitterclone/twitterclone/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 from decouple import config
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -130,3 +131,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+AUTH_USER_MODEL = 'account.User'
+
